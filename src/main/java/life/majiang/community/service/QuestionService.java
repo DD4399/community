2b30dec206800c67ce3a5875884b11ab115e8558
DD4399 <957232455@qq.com>
2020-03-02 17:46:13 +0800

    pageable is diff ao

--- src/main/java/life/majiang/community/service/QuestionService.java
+++ src/main/java/life/majiang/community/service/QuestionService.java
@@ -1,5 +1,6 @@
 package life.majiang.community.service;
 
+import life.majiang.community.dto.PaginationDTO;
 import life.majiang.community.dto.QuestionDTO;
 import life.majiang.community.mapper.QuestionMapper;
 import life.majiang.community.mapper.UserMapper;
@@ -21,9 +22,16 @@ public class QuestionService {
     @Autowired
     private QuestionMapper questionMapper;
 
-    public List<QuestionDTO> list() {
-        List<Question> questions = questionMapper.list(); //拿到所有的Question
+    public PaginationDTO list(Integer page, Integer size) {
+
+        Integer offset = size * (page - 1);
+
+        List<Question> questions = questionMapper.list(offset, size);
         List<QuestionDTO> questionDTOList = new ArrayList<>();
+
+        PaginationDTO paginationDTO = new PaginationDTO();
+        Integer totalPage;
+
         for(Question question : questions) {
             User user = userMapper.findById(question.getCreator());
             //通过PublishController的question.setCreator(user.getId())取到了用户的id
@@ -34,6 +42,22 @@ public class QuestionService {
             questionDTO.setUser(user); //组装最后一个数据
             questionDTOList.add(questionDTO); //放进List里，完成了数据的组装
         }
-        return questionDTOList;
+
+        Integer totalCount = questionMapper.count();
+        if (totalCount % size == 0) {
+            totalPage = totalCount / size;
+        } else {
+            totalPage = totalCount / size + 1;
+        }
+
+        if (page < 1) {
+            page = 1;
+        }
+        if (page > totalPage) {
+            page = totalPage;
+        }
+        paginationDTO.setQuestions(questionDTOList);
+        paginationDTO.setPagination(totalPage, page);
+        return paginationDTO;
     }
 }
